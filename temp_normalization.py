94dc8dd32c35e13f07d3f24cd0b9be39b8aa0d70
Sebastian Calleja <72355114+sebcalleja@users.noreply.github.com>
2022-02-02 07:25:17 -0700

    Update temp_normalization.py
    added statsmodel library

--- temp_normalization.py
+++ temp_normalization.py
@@ -20,6 +20,7 @@ import sklearn
 from sklearn.linear_model import LinearRegression  # to build a LR model for comparison
 #import plotly.graph_objects as go  # for data visualization
 #import plotly.express as px  # for data visualization
+import statsmodel
 import statsmodels.api as sm  # to build a LOWESS model
 from scipy.interpolate import interp1d  # for interpolation of new data points
 import scipy
